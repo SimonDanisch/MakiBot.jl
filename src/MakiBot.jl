a9d3a8e9b94115a1395430bcfcbc50629ec94c94
SimonDanisch <sdanisch@gmail.com>
2017-10-20 15:05:15 +0200

    spice things up

--- src/MakiBot.jl
+++ src/MakiBot.jl
@@ -1,8 +1,9 @@
 module MakiBot
 
 using PyCall, MakiE
-
 @pyimport telepot
+
+include("samples.jl")
 
 function get_user(msg)
     if haskey(msg, "from")
@@ -27,28 +28,35 @@ function process_text(text)
     result
 end
 
+function process_julia(userid, bot, text)
+    try
+        result = process_text(text)
+        if isa(result, Scene)
+            mktempdir() do path
+                name = joinpath(path, "plot.png")
+                save(name, result)
+                bot[:sendPhoto](userid, pybuiltin("open")(name, "rb"))
+            end
+        elseif isa(result, MakiE.VideoStream)
+            vid = MakiE.finish(result, "gif")
+            bot[:sendVideo](userid, pybuiltin("open")(vid, "rb"))
+        end
+    catch e
+        println(e)
+        bot[:sendMessage](userid, string(e))
+    end
+end
+
 function process_message(bot, msg)
     isa(msg, Bool) && return
     userid = get_user(msg)
     text = msg["text"]
     println(text)
     if startswith(text, "#julia")
-        try
-            result = process_text(text)
-            if isa(result, Scene)
-                mktempdir() do path
-                    center!(result)
-                    name = joinpath(path, "plot.png")
-                    save(name, result)
-                    bot[:sendPhoto](userid, pybuiltin("open")(name, "rb"))
-                end
-            elseif isa(result, MakiE.VideoStream)
-                vid = MakiE.finish(result, "mp4")
-                bot[:sendVideo](userid, pybuiltin("open")(vid, "rb"))
-            end
-        catch e
-            println(e)
-            bot[:sendMessage](userid, string(e))
+        process_julia(userid, bot, text)
+    elseif text == "/start"
+        for elem in all_samples
+            process_julia(userid, bot, elem)
         end
     end
 end
@@ -72,6 +80,7 @@ function run_event_loop(bot = makibot[])
             if isempty(responses)
                 sleep(0.1)
             else
+                bot[:sendMessage](userid, "Welcome to MakiBot! You can now write snippets like:")
                 for response in responses
                     last_id[] = response["update_id"] + 1
                     msg = get_message(response)
