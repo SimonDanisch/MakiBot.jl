f0a93501b51f519468f1e168e2479caad77854c0
SimonDanisch <sdanisch@gmail.com>
2018-06-20 22:44:17 +0200

    server

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,31 @@
-using MakiBot
-using Base.Test
+using OhMyREPL
+ENV["JULIA_REVISE"] = "manual"
+using Revise
 
-# write your own tests here
-@test 1 == 2
+function process_text(text)
+    println(text)
+    result = nothing
+    revise()
+    try
+        result = eval(Main, :(include_string($text)))
+    catch e
+        bt = catch_backtrace()
+        Base.showerror(STDERR, e)
+        Base.show_backtrace(STDERR, bt)
+    end
+    eval(Main, Expr(:call, display, result))
+end
+
+@async begin
+    server = listen(2001)
+    while true
+        sock = accept(server)
+        @async while isopen(sock)
+            try
+                process_text(readstring(sock))
+            catch e
+                println(e)
+            end
+        end
+    end
+end
